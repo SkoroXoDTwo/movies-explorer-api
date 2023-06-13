96efec5b17089f4b0793087da2e986ee2bcbc8b2
Alexey Belkov <lexabelco2013@yandex.ru>
2023-06-14 02:01:12 +0500

    feat: поправить баг

--- utils/validation.js
+++ utils/validation.js
@@ -30,8 +30,8 @@ const validationSignin = celebrate({
 
 const validationUpdateUserProfile = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email(),
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().email(),
   }),
 });
 
